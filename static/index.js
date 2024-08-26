9f6e73b0fad6ac71fbdf77b482602499ba128d4e
Geometry Tryhard <memelite69@gmail.com>
2024-08-26 19:46:50 -0400

    update

---
+++ static/index.js
@@ -0,0 +1,39 @@
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+function getRandomUpTo(max) {
+ return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+  const url = search(address.value, searchEngine.value);
+  location.href = '//dont-sue-me-topvaz.topvazgeo.online/launch.html?domain=' + url;
+
+});
